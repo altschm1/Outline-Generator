b22d05ed87808295fb8ceccf911bd50577166c21
Michael Altschuler <altschm1@tcnj.edu>
2019-03-09 19:27:36 -0500

    finish documentation

--- src/main.py
+++ src/main.py
@@ -1,3 +1,12 @@
+"""
+Michael Altschuler
+Created: 3/9/2019
+Updated: 3/9/2019
+
+main.py
+driver program
+"""
+
 import sys
 import preprocessing
 import vector
@@ -5,17 +14,22 @@ import outline
 import output
 
 def get_input(filename):
+    """get string from filename"""
     f = open(filename)
     result = f.read()
     f.close()
     return result
 
 def preprocess_text(text):
+    """preprocess text
+    NOTE: all duplicate sentences need to be removed
+    """
     sentences = preprocessing.tokenize_to_sentences(text)
     vocab = preprocessing.extract_vocab(text)
     return sentences, vocab
 
 def vectorize(option, sentences, vocab):
+    """convert sentences into vector form"""
     if option.lower() == "bow":
         return vector.convert_bow(sentences, vocab)
     elif option.lower() == "doc2vec":
@@ -24,16 +38,28 @@ def vectorize(option, sentences, vocab):
         raise "Not proper input for option"
 
 def outline_vectors(sentences, vectors):
+    """return Graph structure to heircharchically cluster the sentences"""
     return outline.kmeans_merge_clustering(sentences, vectors, 4)
 
 def create_output(output_file, graph):
+    """create html file"""
     output.create_output(output_file, graph)
 
 def main(filename):
+    """main/driver function"""
+    # get input source
     text = get_input(filename)
+    
+    # preprocess text and extract vocab
     sentences, vocab = preprocess_text(text)
+    
+    # get 2d numpy array
     vectors = vectorize("bow", sentences, vocab)
+    
+    # get outline structure
     graph = outline_vectors(sentences, vectors)
+    
+    # print html file
     create_output("dummy3", graph)
     
     text = get_input(filename)
